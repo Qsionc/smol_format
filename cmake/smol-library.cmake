663eac9cb48b77dcde12d80ed5a43f0ebf36c3d6
qsionc <bartoszjandec@gmail.com>
2021-08-01 14:52:59 +0200

    Fixed smol-library.cmake file and installation folders regarding CMAKE_INSTALL_PREFIX

--- cmake/smol-library.cmake
+++ cmake/smol-library.cmake
@@ -4,8 +4,6 @@ add_library(smormat INTERFACE)
 target_include_directories(smormat INTERFACE
         $<BUILD_INTERFACE:${SMORMAT_INCLUDE_DIR}>)
 
-file(GLOB_RECURSE SMORMAT_INCLUDES ${SMORMAT_INCLUDE_DIR}/*.h)
-
 install(TARGETS smormat
         EXPORT smormat-lib
         INCLUDES DESTINATION ${CMAKE_INSTALL_PREFIX}/include/smormat
@@ -15,9 +13,22 @@ install(TARGETS smormat
 install(EXPORT smormat-lib
         FILE smormat-config.cmake
         NAMESPACE smormat::
-        DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/cmake
+        DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/smormat/cmake
         CONFIGURATIONS Release)
 install(FILES
-        ${SMORMAT_INCLUDES}
+        ${CMAKE_SOURCE_DIR}/include/detail/format_settings.h
+        ${CMAKE_SOURCE_DIR}/include/detail/to_string.h
+        ${CMAKE_SOURCE_DIR}/include/detail/formatting.h
+        DESTINATION ${CMAKE_INSTALL_PREFIX}/include/smormat/detail
+        CONFIGURATIONS Release)
+install(FILES
+        ${CMAKE_SOURCE_DIR}/include/implementation/any_type_const_implementation.h
+        ${CMAKE_SOURCE_DIR}/include/implementation/format_class_implementation.h
+        DESTINATION ${CMAKE_INSTALL_PREFIX}/include/smormat/implementation
+        CONFIGURATIONS Release)
+install(FILES
+        ${CMAKE_SOURCE_DIR}/include/any_type_const.h
+        ${CMAKE_SOURCE_DIR}/include/format.h
+        ${CMAKE_SOURCE_DIR}/include/format_class.h
         DESTINATION ${CMAKE_INSTALL_PREFIX}/include/smormat
         CONFIGURATIONS Release)
