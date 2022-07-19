d84307253dc9c96ef4be2667d3c80d5a2ef2fb91
Bartosz Dec <60384439+Qsionc@users.noreply.github.com>
2022-07-19 12:19:40 +0200

    Update linux-setup.cmake
    when using as submodule there were compiler problems when set compiler to clang

--- cmake/linux-setup.cmake
+++ cmake/linux-setup.cmake
@@ -1,6 +1,5 @@
 set(CMAKE_CXX_STANDARD 20)
 set(UNIT_TEST ON)
-set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_GENERATOR "Unix Makefiles")
 add_compile_options(-Wall -Wextra
         $<$<STREQUAL:CMAKE_BUILD_TYPE,"Debug">:-g>
